e0b5e8349c0196573f45f8cc51c1362c45386dd2
Zizwe <Zizwemkz@gmail.com>
2022-06-11 22:19:27 +0200

    error handling code block

--- MessagePublisher/MessagePublisher/Program.cs
+++ MessagePublisher/MessagePublisher/Program.cs
@@ -7,22 +7,29 @@ using MessagePublisher.PublishEventService;
 
         var publisher = new Publisher();
 
-        while (true)
+while (true)
+{
+    try {
+
+        Console.WriteLine("Please enter subject for message");
+        var subject = Console.ReadLine();
+
+        Console.WriteLine("Please enter the message to send");
+        var content = Console.ReadLine();
+
+        var Date = DateTime.Now.ToString();
+        if (!string.IsNullOrEmpty(content))
+            publisher.SendMessage(new MessageEvent { message = content, subject = subject, date = Date
+            });
+        else
         {
-            Console.WriteLine("Please enter subject for message");
-            var subject = Console.ReadLine();
-
-            Console.WriteLine("Please enter the message to send");
-            var content = Console.ReadLine();
-
-            var Date = DateTime.Now.ToString();
-            if (!string.IsNullOrEmpty(content))
-                publisher.SendMessage(new MessageEvent { message = content, subject = subject, date = Date
-                });
-            else
-            {
-                 Environment.Exit(1);
-            }
+            Environment.Exit(1);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
 }
    
 
