d780b6b483b33f46cc19ba8d7dbde36401efa039
Zizwe <Zizwemkz@gmail.com>
2022-06-11 19:34:46 +0200

    updated program class with publishing functions

--- MessagePublisher/MessagePublisher/Program.cs
+++ MessagePublisher/MessagePublisher/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace MessagePublisher
 {
@@ -6,7 +8,35 @@ namespace MessagePublisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConnection conn;
+            IModel channel;
+
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.Port = 5672;
+            factory.VirtualHost = "/";
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            conn = factory.CreateConnection();
+            channel = conn.CreateModel();
+
+            channel.ExchangeDeclare("streamer_job", "fanout", true, false, null);
+
+            channel.QueueDeclare("Department_1", true, false, false, null);
+
+            channel.QueueBind("Testqueue", "streamer_job", "");
+            channel.QueueBind("Department_1", "streamer_job", "");
+
+            channel.BasicPublish("streamer_job", "", null, Encoding.UTF8.GetBytes("mesage sent currently 12"));
+
+            //channel.QueueDelete("Postexchange");
+            //channel.QueueDelete("Department_1");
+            //channel.QueueDelete("Department_2");
+
+            //channel.Close();
+            //conn.Close();
         }
     }
 }
