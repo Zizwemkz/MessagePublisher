b985d73b5351da37309b0311b0973c5f52833166
Zizwe <Zizwemkz@gmail.com>
2022-06-12 20:46:37 +0200

    Code clean up

--- MessagePublisher/MessagePublisher/Program.cs
+++ MessagePublisher/MessagePublisher/Program.cs
@@ -5,35 +5,35 @@ using MessagePublisher.Models;
 using MessagePublisher.PublishEventService;
 
 
-        var publisher = new Publisher();
+    var publisher = new Publisher();
 
-while (true)
-{
-    try {
-
-        Console.WriteLine("Please enter subject for message");
-        var subject = Console.ReadLine();
-
-        Console.WriteLine("Please enter the message to send");
-        var content = Console.ReadLine();
-
-        var Date = DateTime.Now.ToString();
-        if (!string.IsNullOrEmpty(content))
-        {
-             publisher.SendMessage(new MessageEvent { message = content, subject = subject, date = Date});
-             Console.WriteLine("Message successfully sent...");
-        }
-           
-        else
-        {
-            Environment.Exit(1);
-        }
-      
-    }
-    catch (Exception ex)
+    while (true)
     {
-        Console.WriteLine(ex.Message);
+        try {
+
+            Console.WriteLine("Please enter subject for message");
+            var subject = Console.ReadLine();
+
+            Console.WriteLine("Please enter the message to send");
+            var content = Console.ReadLine();
+
+            var Date = DateTime.Now.ToString();
+            if (!string.IsNullOrEmpty(content))
+            {
+                 publisher.SendMessage(new MessageEvent { message = content, subject = subject, date = Date});
+                 Console.WriteLine("Message successfully sent...");
+            }
+      
+            
+                Console.WriteLine("---------------------------------------------------End of line--------------------------------------------------------");
+                Console.WriteLine(" Press [enter] to exit.");
+                Environment.Exit(0);
+
     }
-}
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
    
 
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The is a console app that publish message events from rabbitmq.
 ## The project
 
 This project consists of one-project :
-- **MessagePublisher**: a console project containing a class and logic to type our message and also classes with the rules for sending and publish the message.
+- **MessagePublisher**: a console project containing a class and logic to enter your message and publish the message.
 - **MessagePublisher.Models**: 
         - **MessageEvent**: a class model that will be stored and send to rabbitMQ.
 - **MessagePublisher.publisher**: 
